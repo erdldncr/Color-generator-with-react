79e7a4171da85800ff60af4b100c486b3465e0ef
erdldncr <erdldncr@gmail.com>
2021-05-26 11:39:01 +0100

    Updates

---
+++ precache-manifest.850371f70e60e7315d26e5d910adf424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a709d2cf16c0f25250f83af8fefa7a4b",
+    "url": "/Color-generator-with-react/index.html"
+  },
+  {
+    "revision": "b7d04119d58780abecc8",
+    "url": "/Color-generator-with-react/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/Color-generator-with-react/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/Color-generator-with-react/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d04119d58780abecc8",
+    "url": "/Color-generator-with-react/static/js/main.d78fbc55.chunk.js"
+  },
+  {
+    "revision": "93fae188af081650b068",
+    "url": "/Color-generator-with-react/static/js/runtime-main.966c01d1.js"
+  }
+]);
